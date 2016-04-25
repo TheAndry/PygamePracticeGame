469023832a83d2f77e2829bff3e8512edd9cb8fe
TheAndry <andrykore@gmail.com>
2016-04-25 22:02:15 +0300

    Cards class work in progress. Added new card.

--- MemoryGame.py
+++ MemoryGame.py
@@ -10,8 +10,9 @@ pygame.display.set_caption('MemoryGame')
 
 card_back = pygame.image.load("./resources/cardBack.png").convert_alpha()
 card_fox = pygame.image.load("./resources/cardFox.png").convert_alpha()
+card_dachshund = pygame.image.load("./resources/cardDachshund.png").convert_alpha() #TODO Fix the picture
 
-allcards = [card_back, card_fox]
+allcards = [card_fox, card_dachshund]
 
 class Card:
     def __init__(self, x, y, picture):
@@ -29,17 +30,41 @@ class Card:
     def drawCard(self, scren):
         scren.blit(self.card, [self.x, self.y])
 
+class Cards:
+    def __init__(self):
+        self.cards1 = []
+        self.cards2 = []
 
-card = Card(10, 10, card_fox)
+    def cardBoard(self): #TODO Finish the function and make it work correctly
+        x = 10
+        y = 10
+        for card in allcards:
+            self.cards1.append(Card(x, y, card))
+            x += 140
+            print(self.cards1) #For testing list
+
+    def mouse(self, pos, click):
+        for cardmouse in self.cards1:
+            cardmouse.mouse(pos, click)
+
+    def drawBoard(self, display):
+        for draw in self.cards1:
+            draw.drawCard(display)
+
+def playGame():
+    pass
+
+kaartid = Cards() #For testing
+kaartid.cardBoard()
 
 while True:
     screen.fill([255, 255, 255])
 
-    card.drawCard(screen)
+    kaartid.drawBoard(screen)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            card.mouse(pygame.mouse.get_pos(), pygame.mouse.get_pressed())
+            kaartid.mouse(pygame.mouse.get_pos(), pygame.mouse.get_pressed())
 
     pygame.display.flip()
