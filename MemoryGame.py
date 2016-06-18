fdc4fcc798b095e91be0194b55478b056cf973ba
TheAndry <andrykore@gmail.com>
2016-06-18 15:02:29 +0300

    Release.

--- MemoryGame.py
+++ MemoryGame.py
@@ -1,30 +1,49 @@
 # -*- coding: utf-8 -*-
-import sys, random, time
+import sys, random
 import pygame
 
 
 __author__ = ('Andry Kõre')
 
 pygame.init()
+pygame.mixer.init()
 WIDTH = 890
 HEIGHT = 660
 gameScreen = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption('MemoryGame')
 
-# TODO: Try to make image thing better
-'''Pictures from https://pixabay.com/'''  # TODO: Need 12 cards
+'''Card pictures from www.pixabay.com'''
 card_back = pygame.image.load('./resources/cardBack.png').convert_alpha()
 card_fox = pygame.image.load('./resources/cardFox.png').convert_alpha()
 card_dachshund = pygame.image.load('./resources/cardDachshund.png').convert_alpha()
 card_moose = pygame.image.load('./resources/cardMoose.png').convert_alpha()
 card_bear = pygame.image.load('./resources/cardBear.png').convert_alpha()
-menu_image = pygame.image.load('./resources/gameMenu.png').convert()  # TODO: Link trophy to info tab
-end_image = pygame.image.load('./resources/gameEnd.png').convert() # Trophy- http://www.flaticon.com/free-icon/trophy_147210
+card_beaver = pygame.image.load('./resources/cardBeaver.png').convert_alpha()
+card_cat = pygame.image.load('./resources/cardCat.png').convert_alpha()
+card_duck = pygame.image.load('./resources/cardDuck.png').convert_alpha()
+card_elephant = pygame.image.load('./resources/cardElephant.png').convert_alpha()
+card_horse = pygame.image.load('./resources/cardHorse.png').convert_alpha()
+card_leopard = pygame.image.load('./resources/cardLeopard.png').convert_alpha()
+card_rabbit = pygame.image.load('./resources/cardRabbit.png').convert_alpha()
+card_squirrel = pygame.image.load('./resources/cardSquirrel.png').convert_alpha()
+menu_image = pygame.image.load('./resources/gameMenu.png').convert()
+end_image = pygame.image.load('./resources/gameEnd.png').convert()
 memory_text = pygame.image.load('./resources/memoryGameText.png').convert_alpha()
+
+music_menu = './sound/menuMusic.wav'
+music_game = './sound/gameMusic.wav'
+sound_win = './sound/win.wav'
+
 
 cardSize = card_back.get_rect().size
 
-allCardsList = [card_fox, card_dachshund, card_bear, card_moose]
+allCardsList = [card_fox, card_dachshund, card_bear, card_moose,
+
+                ]
+'''
+card_beaver, card_cat, card_duck, card_elephant,
+                card_horse, card_leopard, card_rabbit, card_squirrel
+'''
 
 
 def Text(screen, text, size, color, coords):
@@ -44,7 +63,7 @@ class Card:
         clickable = True
         pause = False
 
-    def mouse(self, pos, click):  # TODO: Think how to make hover
+    def mouse(self, pos, click):
         global last
         if not pause:
             if clickable:
@@ -155,8 +174,8 @@ class Status():
 
 
 class Game:
-    def __init__(self, screen):  # TODO: Make this area better
-        global mouseClicks, openedCards  # TODO: Read about global
+    def __init__(self, screen):
+        global mouseClicks, openedCards
         self.currentStatus = Status.menu
         self.currentScreen = screen
         self.screenRect = self.currentScreen.get_rect()
@@ -174,7 +193,7 @@ class Game:
         self.surface = pygame.Surface((WIDTH, HEIGHT))
         self.surface.fill((0, 200, 0))
         self.surfaceAlpha = 0
-        self.ajutised_ajad = []
+        self.currentSound = music_menu
         openedCards = []
         mouseClicks = []
 
@@ -225,7 +244,8 @@ class Game:
     def ifOpened(self):
         if len(openedCards) == len(allCardsList)*2:
             if not self.addTime:
-                self.ajutised_ajad.append(self.playtime)
+                if self.playtime < float(self.readFile()):
+                    self.writeFile(self.playtime)
                 self.addTime = True
             self.currentStatus = Status.fade
 
@@ -238,6 +258,17 @@ class Game:
                 seconds = milliseconds / 1000
                 self.playtime += seconds
                 self.t1 = 0
+
+    def writeFile(self, score):
+        file = open('scores.txt', 'w')
+        file.write(str(score))
+        file.close()
+
+    def readFile(self):
+        file = open('scores.txt', 'r')
+        score = file.readline()
+        file.close()
+        return score
 
     def showMenu(self):
         global buttons
@@ -252,12 +283,14 @@ class Game:
         del mouseClicks[:]
         buttons = Allbuttons([
             Button(self.screenRect.centerx-50, self.screenRect.centery-80, 40, 100, 'PLAY', (0,210,0), (10,240,110), (160,245,225), self.startGame),
-            Button(self.screenRect.centerx-50, self.screenRect.centery-30, 40, 100, 'INFO', (0,210,0), (10,240,110), (160,245,225), self.infoTab),  # TODO: Make info tab
+            Button(self.screenRect.centerx-50, self.screenRect.centery-30, 40, 100, 'INFO', (0,210,0), (10,240,110), (160,245,225), self.infoTab),
             Button(self.screenRect.centerx-50, self.screenRect.centery+20, 40, 100, 'EXIT', (0,210,0), (10,240,110), (160,245,225), self.quitGame)
             ])
-        #pygame.mixer.music.stop()
-        #pygame.mixer.music.load(BACKGROUND MUSIC)
-        #pygame.mixer.music.play(-1)
+        if self.currentSound == music_game:
+            pygame.mixer.music.stop()
+            self.currentSound = music_menu
+            pygame.mixer.music.load(music_menu)
+            pygame.mixer.music.play(-1)
 
     def startGame(self):
         global buttons
@@ -268,9 +301,10 @@ class Game:
             Button(725, 25, 30, 70, 'PAUSE', (0,210,00), (10,240,110), (160,245,225), self.goPause, 26),
             Button(640, 25, 30, 70, 'BACK', (0,210,00), (10,240,110), (160,245,225), self.showMenu, 26)
             ])
-        #pygame.mixer.music.stop()
-        #pygame.mixer.music.load(BACKGROUND MUSIC WHILE PLAYING)
-        #pygame.mixer.music.play(-1)
+        pygame.mixer.music.stop()
+        self.currentSound = music_game
+        pygame.mixer.music.load(self.currentSound)
+        pygame.mixer.music.play(-1)
 
     def infoTab(self):
         global buttons
@@ -282,10 +316,11 @@ class Game:
     def goPause(self):
         global pause
         if pause:
+            pygame.mixer.music.unpause()
             pause = False
         else:
+            pygame.mixer.music.pause()
             pause = True
-        #pygame.mixer.music.pause()
 
     def goFade(self):
         global clickableButton
@@ -296,6 +331,10 @@ class Game:
         clickableButton = False
         if self.surfaceAlpha == 255:
             self.gameEnd()
+        sound = pygame.mixer.Sound(sound_win)
+        sound.set_volume(.4)
+        sound.play()
+
 
     def gameEnd(self):
         global buttons, clickableButton
@@ -305,21 +344,23 @@ class Game:
             Button(self.screenRect.centerx-135, 580, 40, 90, 'MENU', (0,200,0), (10,240,110), (160,245,225), self.showMenu),
             Button(self.screenRect.centerx+45, 580, 40, 90, 'EXIT', (0,200,0), (10,240,110), (160,245,225), self.quitGame)
         ])
-        #sound = pygame.mixer.Sound(GAME END SOUND)
-        #sound.play()
-        #pygame.mixer.music.stop()
+        pygame.mixer.music.stop()
+        self.currentSound = music_menu
+        pygame.mixer.music.load(music_menu)
+        pygame.mixer.music.play(-1)
 
     def quitGame(self):
         pygame.quit()
         sys.exit()
 
     def updateScreen(self):
-        global buttons, last
+        global buttons
         self.now = self.clock.tick()
 
         if self.currentStatus == Status.menu:
             self.currentScreen.blit(menu_image, (0, 0))
             buttons.drawbutton(self.currentScreen)
+
             pygame.display.flip()
 
         elif self.currentStatus == Status.info:
@@ -327,9 +368,21 @@ class Game:
             buttons.drawbutton(self.currentScreen)
             self.currentScreen.blit(memory_text, (self.screenRect.centerx-120, 20))
             Text(self.currentScreen, 'INFO', 40, (0, 0, 0), (self.screenRect.centerx-20, 75))
+            Text(self.currentScreen, 'Licences', 40, (200, 0, 0), (30, 110))
+            Text(self.currentScreen, 'Card pictures from - www.pixabay.com', 30, (0, 0, 0), (30, 150))
+            Text(self.currentScreen, 'Menu picture from - www.pixabay.com', 30, (0, 0, 0), (30, 180))
+            Text(self.currentScreen, 'Game end trophy from - www.flaticon.com', 30, (0, 0, 0), (30, 230))
+            Text(self.currentScreen, 'Trophy author: Freepik', 30, (0, 0, 0), (30, 260))
+            Text(self.currentScreen, 'Trophy address: http://www.flaticon.com/free-icon/trophy_147210', 30, (0, 0, 0), (30, 290))
+            Text(self.currentScreen, 'Menu music from - www.bensound.com', 30, (0, 0, 0), (30, 340))
+            Text(self.currentScreen, 'Game music from - www.orangefreesounds.com', 30, (0, 0, 0), (30, 370))
+            Text(self.currentScreen, 'Winning sound from - www.soundbible.com', 30, (0, 0, 0), (30, 400))
+            Text(self.currentScreen, 'Game created by - Andry Kõre', 30, (0, 200, 0), (30, 460))
+            Text(self.currentScreen, 'This game is made to learn Python and Pygame.', 30, (0, 200, 0), (30, 480))
+            Text(self.currentScreen, 'Release - 18.06.2016', 30, (0, 200, 0), (30, 500))
             pygame.display.flip()
 
-        elif self.currentStatus == Status.game or self.currentStatus == Status.fade:  # Make time tick
+        elif self.currentStatus == Status.game or self.currentStatus == Status.fade:
             buttons.drawbutton(self.currentScreen)
             self.currentScreen.blit(memory_text, (10, 20))
             pygame.draw.line(self.currentScreen, [0, 0, 0], [0, 70], [WIDTH, 70])
@@ -349,14 +402,16 @@ class Game:
         elif self.currentStatus == Status.end:
             self.currentScreen.blit(end_image, (0, 0))
             buttons.drawbutton(self.currentScreen)
-            Text(self.currentScreen, 'Your time: '+str(round(self.ajutised_ajad[-1], 1)), 40, (0, 200, 0), (self.screenRect.centerx-90, 400))
-            Text(self.currentScreen, 'Best time: '+str(round(min(self.ajutised_ajad), 1)), 40, (200, 0, 0), (self.screenRect.centerx-90, 450))
+            Text(self.currentScreen, 'Your time: '+str(round(self.playtime, 1)), 40, (0, 200, 0), (self.screenRect.centerx-90, 400))
+            Text(self.currentScreen, 'Best time: '+str(round(float(self.readFile()), 1)), 40, (200, 0, 0), (self.screenRect.centerx-90, 450))
 
             pygame.display.flip()
 
 
 game = Game(gameScreen)
 game.showMenu()
+pygame.mixer.music.load(music_menu)
+pygame.mixer.music.play(-1)
 
 while True:
     for event in pygame.event.get():
